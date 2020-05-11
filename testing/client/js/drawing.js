7ba53c2f848274caee63e99107be7a2640000a7f
HiWiSciFi <max.j.hager@gmail.com>
2020-05-11 17:13:52 +0200

    fixed major soket.io isseues in testing

--- testing/client/js/drawing.js
+++ testing/client/js/drawing.js
@@ -1,72 +1,4 @@
-var mousePressed = false;
-var lastX, lastY;
-var context;
-var canvas;
 
-var enabled = true;
-
-const canvasID = "drawingCanvas";
-
-const typePencil = "pencil";
-const typeBucket = "bucket";
-const typeEraser = "eraser";
-const widthID = "setLWidth";
-
-var selectedColor = '#000000';
-var selectedBrush = 'pencil';
-
-
-function InitThis() {
-	canvas = document.getElementById(canvasID);
-	context = canvas.getContext("2d");
-
-	canvas.addEventListener("mousedown", mousedown);
-	canvas.addEventListener("mousemove", mousemove);
-	canvas.addEventListener("mouseup", mouseup);
-	canvas.addEventListener("mouseleave", mouseleave);
-	canvas.addEventListener("mouseenter", mouseenter);
-
-	context.fillStyle = '#FFFFFF';
-    context.fillRect(0, 0, canvas.width, canvas.height);
-
-	context.imageSmoothingEnabled = false;
-}
-
-function mousedown(e) {
-	if (enabled) {
-	  	if (selectedBrush === typeBucket) {
-			floodFill(e.pageX - canvas.offsetLeft, e.pageY - canvas.offsetTop, hexToRgbA(selectedColor));
-		} else if (selectedBrush === typePencil || selectedBrush === typeEraser) {
-    		mousePressed = true;
-    		Draw(e.pageX - canvas.offsetLeft, e.pageY - canvas.offsetTop, false);
-		}
-	}
-}
-
-function mousemove(e) {
-	if (enabled && mousePressed && (selectedBrush === typePencil || selectedBrush === typeEraser)) {
-    	Draw(e.pageX - canvas.offsetLeft, e.pageY - canvas.offsetTop, true);
-	}
-}
-
-function mouseup(e) {
-	if (enabled) {
-    	mousePressed = false;
-	}
-}
-
-function mouseleave(e) {
-	if (enabled) {
-    	mousePressed = false;
-	}
-}
-
-function mouseenter(e) {
-	if (enabled && e.buttons === 1 && (selectedBrush === typePencil || selectedBrush === typeEraser)) {
-	  	mousePressed = true;
-		Draw(e.pageX - canvas.offsetLeft, e.pageY - canvas.offsetTop, false);
-	}
-}
 
 function Draw(x, y, isDown) {
 	if (isDown) {
@@ -238,35 +170,6 @@ function compareColors(first, second) {
 		return true;
 	}
 	return false;
-}
-
-function clearArea() {
-	context.setTransform(1, 0, 0, 1, 0, 0);
-	context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-}
-
-function selectColor(hex) {
-	console.log("selected color: " + hex);
-
-	var btn = document.getElementById("color_" + selectedColor);
-	btn.setAttribute("class", "color-unselected");
-
-	btn = document.getElementById("color_" + hex);
-	btn.setAttribute("class", "color-selected");
-
-	selectedColor = hex;
-}
-
-function selectBrush(brush) {
-	console.log("selected brush: " + brush);
-
-	var btn = document.getElementById("brush_" + selectedBrush);
-	btn.setAttribute("class", "brush-unselected");
-
-	btn = document.getElementById("brush_" + brush);
-	btn.setAttribute("class", "brush-selected");
-
-	selectedBrush = brush;
 }
 
 function hexToRgbA(hex) {
